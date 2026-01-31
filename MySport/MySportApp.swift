19294279eb686a716199fdb8568cb1831389ec3c
Yevheniia-Ivanets <146843337+Yevheniia-Ivanets@users.noreply.github.com>
2026-02-01 00:24:09 +0100

    Initial Commit

---
+++ MySport/MySportApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySportApp.swift
+//  MySport
+//
+//  Created by Evgeniya Ivanets on 01.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MySportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
